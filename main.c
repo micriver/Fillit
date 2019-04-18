d37fb5b6395c3e2dd2bbd2d0ad9299473450b2d5
Michael Rivera <mirivera@e1z2r3p29.42.us.org>
2019-04-18 14:45:15 -0700

    pushing changes

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mirivera <mirivera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 10:30:12 by mirivera          #+#    #+#             */
-/*   Updated: 2019/04/17 20:02:28 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/04/18 14:44:36 by mirivera         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,15 +28,19 @@ int		main(int ac, char **av)
 
 		i = 0;
 		fd = open(av[1], O_RDONLY);
-		while (i <= 4)
+		while (i <= 3)
 		{
 			get_next_line(fd, &line);
-			printf("%s\n", line);
-			validate_tetro(line);
+			// if ((get_next_line(fd, &line) == 0))
+			// 	return (0);
+			if (validate_tetro(line) == 0)
+				ERROR;
 			i++;
 		}
-		
-		// if (ft_strchr(line, '\n'))
+		if (line == NULL)
+				printf("this is a new line\n");
+		printf("%s\n", line);
+		// if (i % 4 != 0)
 		// {
 		// 	ft_putstr(ERROR);
 		// 	return (0);
@@ -60,27 +64,36 @@ int		validate_tetro(char *str)
 	int i;
 	int dotcount;
 	int sharpcount;
-	int nlcount;
 	
 	i = 0;
 	dotcount = 0;
 	sharpcount = 0;
-	nlcount = 0;
 	//charcountcheck can be in another function
 	if (ft_strlen(str) != 4)
-		ERROR;
-	while (str[i] == '.' || str[i] == '#' || str[i] == '\n')
+		return (0);
+	//This loop checks for invalid chars
+	while (str[i])
 	{
-		if (str[i] == '.')
-			dotcount++;
-		if (str[i] == '#')
-			sharpcount++;
-		if (str[i] == '\n')
-			nlcount++;
+		if (str[i] != '.' && str[i] != '#')
+			return (0);
+		//This loop counts the number of sharps but since validate tetro
+		//looks onyl at one line at a time, we can't count the number of sharps
+		//across the whole tetrimino just yet
+		// if (str[i] == '#')
+		// {
+		// 	sharpcount++;
+		// 	if (sharpcount > 4)
+		// 		return (0);
+		// }	
 		i++;
 	}
-	printf("%d\n", dotcount);
-	printf("%d\n", sharpcount);
-	printf("%d\n", nlcount);
-	return (0);
+	// i = 0;
+	// while (str[i])
+	// {
+
+	// 	i++;
+	// }
+	
+	//printf("%d dots, %d sharps\n", dotcount, sharpcount);
+	return (1);
 }
