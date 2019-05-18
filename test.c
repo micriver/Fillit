0ee32bca733001550224fc54fd2a3d9896eac6c7
Michael Rivera <mirivera@e1z2r3p29.42.us.org>
2019-05-17 21:14:10 -0700

    norminette and clean up on solver file

--- test.c
+++ test.c
@@ -3,7 +3,64 @@
 
 int size = 3;
 
-int    drop(char *piece, char **board, int j)
+int		drop(char *piece, char **board, int j)
+{
+	int i;
+	int c;
+	int k;
+	int x;
+
+	i = 0;
+	c = 0;
+	k = 0;
+	while (piece[k] && piece[k] < 64) //ie, a letter above 64 in ascii
+		k++;
+	while (board[i][j] != '.' && board[i][j]) //while we're at a character from the previous piece's placement 
+	{
+		j++; //iterate through the board's chars
+		if (!board[i][j])
+		{
+			i++;
+			j = 0;
+		}
+	}
+	while (board[i] && piece[k])
+	{
+		if (piece[k] >= 'A' && piece[k] <= 'Z' && board[i][j] != '.')
+			return (0);
+		if (piece[k] >= 'A' && piece[k] <= 'Z' && board[i][j] == '.')
+		{
+			board[i][j] = piece[k];
+			c++;
+		}
+		if (c == 4)
+			return (1);
+		j++;
+		if (piece[k] == '\n')
+		{
+			x = 0;
+			while (x < size - 4)
+			{
+				if (!board[i][j])
+				{
+					i++;
+					j = 0;
+				}
+				j++;
+				x++;
+			}
+		}
+		k++;
+		if (!board[i][j])
+		{
+			i++;
+			j = 0;
+		}
+	}
+	return (0);
+}
+
+/* int		drop(char *piece, char **board, int j)
 {
 	int i = 0;
 	int c = 0;
@@ -44,26 +101,51 @@ int    drop(char *piece, char **board, int j)
 		}
 	}
 	return (0);
-}
+} */
 
-void	pickup(char** board, char c) {
-	for (int i = 0; board[i]; i++)
-		for (int j = 0; board[i][j]; j++)
+
+void	pickup(char **board, char c)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (board[i])
+	{
+		j = 0;
+		while (board[i][j])
+		{
 			if (board[i][j] == c)
 				board[i][j] = '.';
+			j++;
+		}
+		i++;
+	}
 }
 
-int solve(char **board, char **pieces) {
-	char letter;
+int		solve(char **board, char **pieces)
+{
+	int		i;
+	int		j;
+	char	letter;
+
+	i = -1;
 	if (!pieces[0])
 		return (1);
-	for (int i = 0; pieces[0][i]; i++)
-		if (pieces[0][i] >= 64) {
+	while (pieces[0][++i])
+	{
+		if (pieces[0][i] >= 64)
+		{
 			letter = pieces[0][i];
 			break ;
 		}
-	for (int i = 0; board[i]; i++) {
-		for (int j = 0; board[i][j]; j++) {
+	}
+	i = -1;
+	while (board[++i])
+	{
+		j = -1;
+		while (board[i][++j])
+		{
 			if (!drop(pieces[0], board, j) || !solve(board, &pieces[1]))
 				pickup(board, letter);
 			else
@@ -73,22 +155,34 @@ int solve(char **board, char **pieces) {
 	return (0);
 }
 
-char **builder(int size) {
-    size += 1;
-    char **board = (char**)malloc(sizeof(char*) * size);
-        for (int i = 0; i < size - 1; i++) {
-            board[i] = (char*)malloc(sizeof(char) * size + 1);
-            for (int j = 0; j < size - 1; j++)
-                board[i][j] = '.';
-            board[i][size - 1] = '\n';
-            board[i][size] = '\0';
-        }
-    board[size - 1] = NULL;
-    return (board);
+char	**builder(int size)
+{
+	int		i;
+	int		j;
+	char	**board;
+
+	i = 0;
+	size += 1;
+	board = (char **)malloc(sizeof(char *) * size);
+	while (i < (size - 1))
+	{
+		board[i] = (char*)malloc(sizeof(char) * size + 1);
+		j = 0;
+		while (j < (size - 1))
+		{
+			board[i][j] = '.';
+			j++;
+		}
+		board[i][size - 1] = '\n';
+		board[i][size] = '\0';
+		i++;
+	}
+	board[size - 1] = NULL;
+	return (board);
 }
 
 int main() {
-	char **pieces =  (char**)malloc(sizeof(char*) * 6);
+	char **pieces = (char**)malloc(sizeof(char*) * 6);
 	char **board = builder(size);
 	pieces[0] = "A...\nA...\nA...\nA...\n";
 	pieces[1] = "...B.\n.BBB\n....\n....\n";
