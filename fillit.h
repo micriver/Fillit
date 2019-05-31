948c1a380649172e4f998fe8c4d7ee6068ad39a9
Brandon Feltz <brfeltz@e1z2r3p31.42.us.org>
2019-05-31 13:56:16 -0700

    cleaned and normed header

--- fillit.h
+++ fillit.h
@@ -6,7 +6,7 @@
 /*   By: mirivera <mirivera@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/17 13:10:02 by mirivera          #+#    #+#             */
-/*   Updated: 2019/05/29 17:59:44 by mirivera         ###   ########.fr       */
+/*   Updated: 2019/05/31 13:54:04 by brfeltz          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,48 +33,26 @@ int		g_size;
 ** Validation
 */
 
+int		placement_check(char *piece, char c, int x);
 int		checkchars(char *str, int d, int s, int nl);
-int		checksides(char *str);
+int		checksides(char *str, char letter);
 char	**piece_check(char **str);
 
 /*
 ** Board management
 */
 
-void	convert_to_char(char *str, char c);
-// char	**builder(int g_size);
-// void	print_board(char **board, int size);
-
-/*
-** Solve
-*/
-
-// int		place(char *piece, char **board, int j, int c);
-// void	    pickup(char **board, char c);
-// int		solve(char **board, char **pieces);
-// int		backtrack(char **board, char **pieces, char c);
-
-/*
-** Version3, back to single string board attempt!
-*/
-
+void	convert_to_char(char **str, char c);
 void	print_board(char *board);
-char	*builder(int g_size);
+void	builder(char *board, char **pieces, int size);
 
-int		solve(char *board, char **pieces);
+/*
+** Piece Solve & Drop
+*/
+
+int		solve(char *board, char **pieces, int i);
 int		backtrack(char *board, char **pieces, char letter);
-int		place(char *piece, char *board);
-int		placement_check(char *piece, char c);
-void    pickup(char *board, char c);
-
-
-
-
-
-
-
-
-
-
+int		place(char *piece, char *board, int i);
+void	pickup(char *board, int c);
 
 #endif
